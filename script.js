b7a8dd618536e92119e03548955139d466154fe8
Slum-toto <programingtmaster@hotmail.com>
2025-12-03 10:07:46 +0300

    cooerce

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Main script for HECO-Africa Shop
 const products = [
   { id:1, name:"kaftan shirt", category:"Kente", price:4500, image:"images/dad 1.jpg", rating:4, isNew:true },
-  { id:2, name:"Oga shirt", category:"Agbada", price:7000, image:"images/dad 2.jpg", rating:5, isNew:true },
-  { id:3, name:"west african dress", category:"Ankara", price:3200, image:"images/martin 3.jpg", rating:4 },
-  { id:4, name:"kids west african shirt", category:"Ankara", price:2800, image:"images/martin 2.jpg", rating:3 },
-  { id:5, name:"African Hoodie", category:"Casual", price:2500, image:"images/mama 2.jpg", rating:4 },
-  { id:6, name:"Printed Tee", category:"Casual", price:1200, image:"images/mama 1.jpg", rating:3 },
-  { id:7, name:"Blue Agbada Jacket", category:"Agbada", price:7000, image:"images/dad 50.jpg", rating:5 },
+  { id:2, name:"Oga shirt", category:"Agbada", price:4500, image:"images/dad 2.jpg", rating:5, isNew:true },
+  { id:3, name:"west african dress", category:"Ankara", price:7500, image:"images/martin 3.jpg", rating:4 },
+  { id:4, name:"kids west african shirt", category:"Ankara", price:1500, image:"images/martin 2.jpg", rating:3 },
+  { id:5, name:"west african kitenge", category:"Casual", price:7500, image:"images/mama 2.jpg", rating:4 },
+  { id:6, name:"Printed Tee", category:"Casual", price:6800, image:"images/mama 1.jpg", rating:3 },
+  { id:7, name:"Blue Agbada Jacket", category:"Agbada", price:4500, image:"images/dad 50.jpg", rating:5 },
   { id:8, name:"Agbada Pattern Shirt", category:"Agbada", price:6500, image:"images/dera 1.jpg", rating:4 },
-  { id:9, name:"Kente Scarf", category:"Kente", price:1500, image:"images/Heco1.2.jpg", rating:4 },
-  { id:10, name:"Kente Scarf", category:"Kente", price:1500, image:"images/dera 2.jpg", rating:5 }
+  { id:9, name:"Kente Scarf", category:"Kente", price:7500, image:"images/Heco1.2.jpg", rating:4 },
+  { id:10, name:"Kente Scarf", category:"Kente", price:6500, image:"images/dera 2.jpg", rating:5 },
+   { id:10, name:"Kente Scarf", category:"Kente", price:4500, image:"images/osore.jpg", rating:5 },
 ];
 
 let cart = JSON.parse(localStorage.getItem('st_cart') || '[]');
